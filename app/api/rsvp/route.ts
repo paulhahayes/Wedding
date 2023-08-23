0bf7f4cafeb18fa3e0ddfca0e104b8cd7d2296f5
z5303576 <z5303576@ad.unsw.edu.au>
2023-08-23 13:19:53 +1000

    contact form rework

--- app/api/rsvp/route.ts
+++ app/api/rsvp/route.ts
@@ -1,32 +1,43 @@
 import { supabase } from "@/lib/initSupabase";
 
-export async function POST(req: Request) {
-  const body = await req.json();
-  const { firstName, lastName, attending } = body;
-
+async function registerGuestInDB(guest: {
+  firstName: string;
+  lastName: string;
+  attending: boolean;
+}) {
   try {
-    // Insert the data into the "guests" table
-    const { data, error } = await supabase.from("guests").insert([
+    await supabase.from("guests").insert([
       {
-        first_name: firstName,
-        last_name: lastName,
-        attending: attending,
+        first_name: guest.firstName,
+        last_name: guest.lastName,
+        attending: guest.attending,
       },
     ]);
-
-    // Handle any errors that occurred during the insertion
-    if (error) {
-      console.error("Error inserting data:", error);
-      return new Response("Error", { status: 500 });
-    }
-
-    // Data was successfully inserted
-    console.log("Data inserted successfully:", data);
-
-    // Respond with a success message
     return new Response("OK", { status: 200 });
   } catch (error) {
     console.error("An unexpected error occurred:", error);
     return new Response("Error", { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { firstName, lastName, attending } = body;
+
+  const guestOne = {
+    firstName,
+    lastName,
+    attending,
+  };
+  registerGuestInDB(guestOne);
+
+  if (body.plusone) {
+    const { plusoneFirstName, plusoneLastName, plusoneAttending } = body;
+    const guestTwo = {
+      firstName: plusoneFirstName,
+      lastName: plusoneLastName,
+      attending: plusoneAttending,
+    };
+    registerGuestInDB(guestTwo);
   }
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,43 +1,180 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { Button } from "@material-tailwind/react";
+import { RiFileCopyLine } from "react-icons/ri";
+import { sendContactForm } from "@/lib/api";
+
+//TODO TOAST, isLoading, Mobile
 
 const Contact = () => {
-  const handleClick = () => {
-    console.log("clicked");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    sendContactForm(data);
   };
 
+  const copied = () => toast("Copied ✅");
+
   return (
-    <div className="m-24 pb-8 borde ">
-      <div className="flex items-center gap-8 ">
-        <div className="flex-1 h-[500px] relative ">
-          <Image
-            src="/contact.png"
-            alt=""
-            fill={true}
-            className="object-contain "
-          />
+    <div className=" min-[390px]:pb-14 sm:pb-8">
+      <Toaster />
+      <div className="flex min-[390px]:flex-col sm:flex-row gap-8 min-[330px]:mx-4 min-[330px]:justify-center">
+        <div className="flex-1 sm:h-[600px] flex flex-col gap-[20px] items-end ">
+          <div className="w-full h-32 flex items-center flex-row px-8 gap-3 backdrop-blur-3xl rounded bg-slate-700/30 border-white border text-white sm:text-[20px] min-[330px]:text-[12px] font-bold">
+            <CopyToClipboard text="0402502689" onCopy={copied}>
+              <div
+                className="relative 
+                              group 
+                              flex 
+                              items-center 
+                              rounded-md 
+                              w-full
+                              gap-x-4 
+                              h-[64px]
+                              bg-neutral-100/10 
+                              cursor-pointer 
+                              hover:bg-neutral-100/20 
+                              transition 
+                              pr-4"
+              >
+                <div className="relative min-h-[64px] min-w-[64px]">
+                  <Image
+                    className="object-cover rounded-l"
+                    src="/c.png"
+                    fill
+                    alt="Image"
+                  />
+                </div>
+                <p className="text-[20px] font-bold truncate py-5">
+                  0402 502 689
+                </p>
+                <div
+                  className="
+                  absolute 
+                  transition 
+                  opacity-0 
+                  rounded-full 
+                  flex 
+                  items-center 
+                  justify-center 
+                  bg-blue-300 
+                  p-4 
+                  drop-shadow-md 
+                  right-5
+                  group-hover:opacity-100 
+                  hover:scale-110
+                  "
+                >
+                  <RiFileCopyLine />
+                </div>
+              </div>
+            </CopyToClipboard>
+          </div>
+
+          <div className="w-full h-32 flex items-center flex-row px-8 gap-3 backdrop-blur-3xl rounded bg-slate-700/30 border-white border text-white text-[20px] font-bold">
+            <CopyToClipboard text="giseth21@gmail.com" onCopy={copied}>
+              <div
+                className="relative 
+                              group 
+                              flex 
+                              items-center 
+                              rounded-md 
+                              w-full
+                              gap-x-4 
+                              h-[64px]
+                              bg-neutral-100/10 
+                              cursor-pointer 
+                              hover:bg-neutral-100/20 
+                              transition 
+                              pr-4"
+              >
+                <div className="relative min-h-[64px] min-w-[64px]">
+                  <Image
+                    className="object-cover rounded-l"
+                    src="/email.png"
+                    fill
+                    alt="Image"
+                  />
+                </div>
+                <p className="sm:text-[20px] min-[390px]:text-[18px] font-bold truncate py-5">
+                  Giseth21@gmail.com
+                </p>
+                <div
+                  className="
+                  absolute 
+                  transition 
+                  opacity-0 
+                  rounded-full 
+                  flex 
+                  items-center 
+                  justify-center 
+                  bg-blue-300 
+                  p-4 
+                  drop-shadow-md 
+                  right-5
+                  group-hover:opacity-100 
+                  hover:scale-110
+                  "
+                >
+                  <RiFileCopyLine />
+                </div>
+              </div>
+            </CopyToClipboard>
+          </div>
         </div>
-        <form className="flex-1 flex flex-col gap-[20px] ">
+        <div className="sm:h-[600px] border"></div>
+
+        <form
+          className="flex-1 flex flex-col gap-[20px] h-[600px] "
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="text"
+            {...register("name", { required: true })}
             placeholder="name"
             className="p-[20px] placeholder-white backdrop-blur-3xl rounded bg-slate-700/30 border-none outline-none text-white text-[20px] font-bold"
           />
+          {errors.name && <span>This field is required</span>}
           <input
-            type="text"
+            type="email"
+            {...register("email", { required: true })}
             placeholder="email"
             className="p-[20px] backdrop-blur-3xl  placeholder-white rounded bg-slate-700/30 border-none outline-none text-white text-[20px] font-bold "
           />
+          {errors.email && <span>This field is required</span>}
+          <input
+            type="text"
+            {...register("subject", { required: true })}
+            placeholder="subject"
+            className="p-[20px] placeholder-white backdrop-blur-3xl rounded bg-slate-700/30 border-none outline-none text-white text-[20px] font-bold"
+          />
+          {errors.name && <span>This field is required</span>}
           <textarea
+            {...register("message", { required: true })}
             className="p-[20px] h-[400px] backdrop-blur-3xl rounded placeholder-white bg-slate-700/30 border-none outline-none text-white text-[20px] font-bold"
             placeholder="message"
           ></textarea>
+          {errors.message && <span>This field is required</span>}
+
           <div className="flex justify-end">
             <Button
-              className=" bg-blue-300/70 py-2 px-6 w-[50%] mt-2 border-2 border-white"
-              onClick={handleClick}
+              type="submit"
+              className="bg-blue-300/70 py-2 px-6 w-[50%] mt-2 border-2 border-white"
             >
               Send
             </Button>
