4cc42cba36cbd9bae03d2842efe366d42868345c
paul.work <paul.hayes.k@gmail.com>
2023-09-07 22:04:59 +1000

    laptop change

--- app/registry/layout.tsx
+++ app/registry/layout.tsx
@@ -8,7 +8,6 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="md:px-36 text-white text-center">
       <h1 className="text-[70px]">Registry</h1>
-      <p className="text-lg font-semibold">Here are some gift ideas</p>
       {children}
     </div>
   );
