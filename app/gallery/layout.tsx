09df0cfb39c72316165914997431ccfee3c062a5
z5303576 <z5303576@ad.unsw.edu.au>
2023-09-15 07:46:58 +1000

    fixed spelling mistakes

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -1,13 +1,16 @@
 "use client";
-
+import useTranslate from "@/hooks/useTranslate";
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
+  const { lang } = useTranslate();
   return (
     <div className="md:px-36  py-0 text-center">
-      <h1 className="text-[70px] text-white">Gallery</h1>
+      <h1 className="text-[64px] sm:text-[70px] text-white">
+        {lang === "en" ? "Gallery" : "Galería"}
+      </h1>
       {children}
     </div>
   );
--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -27,7 +27,7 @@ const Timeline = () => {
           time=""
           desc={
             lang === "en"
-              ? "After the ceremony we invite guests to make there way to Chowder Bay. There will be a shuttle bus on the day leaving from the carpark.  Alternatively, there is a small bush walk to the bay. Laslty for those driving, there is parking close to ripples but beware it is metered."
+              ? "After the ceremony we invite guests to make their way to Chowder Bay. There will be a shuttle bus on the day leaving from the carpark.  Alternatively, there is a small bush walk down to the bay. Laslty, for those driving, there is parking close to ripples but beware it is metered."
               : "Después de la ceremonia invitamos a los invitados a dirigirse a Chowder Bay. Habrá un autobús lanzadera ese día que saldrá desde el aparcamiento. Alternativamente, hay un pequeño paseo por los arbustos hasta la bahía. Por último, para quienes conducen, hay estacionamiento cerca de las ondulaciones, pero tengan cuidado porque tiene taxímetro."
           }
           reverse
@@ -42,7 +42,7 @@ const Timeline = () => {
           time=""
           desc={
             lang === "en"
-              ? "There will be a short break between the ceremony and receiption. Feel free to hang out around Chowder Bay and enjoy some drinks, sunshine and good company. Explore the Wharf and take plenty of photos! "
+              ? "There will be a short break between the ceremony and reception. Feel free to hang out around Chowder Bay and enjoy some drinks, sunshine and good company. Explore the Wharf and take plenty of photos! "
               : "Habrá un breve descanso entre la ceremonia y la recepción. Siéntete libre de pasar el rato en Chowder Bay y disfrutar de unas bebidas, el sol y la buena compañía. ¡Explora el muelle y toma muchas fotos!"
           }
           image="Chowder-bay_byw4be"
@@ -54,8 +54,8 @@ const Timeline = () => {
           time="6:30pm"
           desc={
             lang === "en"
-              ? "The official reception begins at 6:30pm. There will be drinks and food."
-              : "La recepción oficial comienza a las 6:30 pm. Habrá bebidas y comida."
+              ? "The official reception begins at 6:30pm. There will be food and drinks."
+              : "La recepción oficial comienza a las 6:30 pm. Habrá comida ybebidas."
           }
           reverse
           image="ripples-day"
